d6c575d7610f4600c34d6e77a070a7e3eda92e90
Emilio Correa <em.correa.soto@gmail.com>
2021-05-25 23:47:08 -0400

    Construyendo las primeras páginas y consultando a la API

---
+++ src/components/coins/CoinsScreen.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import Http from '../../libs/http';
+
+class CoinsScreen extends Component{
+
+    componentDidMount = async () => {
+        const coins = await Http.instance.get("https://api.coinlore.net/api/tickers/");
+        console.log("coins", coins)
+    }
+
+    handlePress = () => {
+        console.log('go to detail', this.props)
+
+        this.props.navigation.navigate('CoinDetail');
+    }
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text style={styles.titleText}>Coins Screen</Text>
+                <Pressable style={styles.btn} onPress={this.handlePress}>
+                    <Text style={styles.btnText}>Ir a details</Text>
+                </Pressable>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "red"
+    },
+    titleText: {
+        color: "#fff",
+        textAlign: "center"
+    },
+    btn: {
+        padding: 8,
+        backgroundColor: "blue",
+        borderRadius: 8,
+        margin: 16
+    },
+    btnText: {
+        color: "#fff",
+        textAlign: "center"
+    }
+});
+
+export default CoinsScreen;
